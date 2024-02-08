f5832b12be1244f0b366e20d222fb9a10aefb514
“Shiwanshu <skumar@hawk.iit.edu>
2024-02-07 20:13:51 -0600

    update wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,5 @@
-from .app import app as application
+from .app import app
 
 
 if __name__ == "__main__":
-    from werkzeug.serving import run_with_reloader
-    run_with_reloader(application)
+    app.run()
